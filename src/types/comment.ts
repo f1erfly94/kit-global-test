7cdb90710f037b13f979596019799677ab3873e9
f1erfly94 <99speedyz99@gmail.com>
2025-07-27 21:05:30 +0300

    fix problems

--- src/types/comment.ts
+++ src/types/comment.ts
@@ -1,21 +1,20 @@
-export namespace Blog {
-    export interface Comment {
-        id: string;
-        postId: string;
-        content: string;
-        author: string;
-        createdAt: string;
-    }
 
-    export interface CreateCommentData {
-        postId: string;
-        author: string;
-        content: string;
-    }
+export interface BlogComment {
+    id: string;
+    postId: string;
+    content: string;
+    author: string;
+    createdAt: string;
+}
 
-    export interface CommentsState {
-        items: Comment[];
-        loading: boolean;
-        error: string | null;
-    }
+export interface CreateBlogCommentData {
+    postId: string;
+    author: string;
+    content: string;
+}
+
+export interface BlogCommentsState {
+    items: BlogComment[];
+    loading: boolean;
+    error: string | null;
 }
